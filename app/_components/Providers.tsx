bcb3fd03d35ec35ad541373faf8295536893217e
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-03-26 16:08:01 +0530

    design landing page

--- app/_components/Providers.tsx
+++ app/_components/Providers.tsx
@@ -2,18 +2,12 @@
 
 import { SessionProvider } from "next-auth/react"
 import { ReactNode } from "react"
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 
 const Providers = ({children}:{ children: ReactNode}) => {
-    const queryClient = new QueryClient()
 
     return (
         <SessionProvider>
-            <QueryClientProvider client={queryClient}>
-                {children}
-                <ReactQueryDevtools initialIsOpen={false} />
-            </QueryClientProvider>
+            {children}
         </SessionProvider>
     )
 }
